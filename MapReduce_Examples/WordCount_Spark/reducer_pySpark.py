45f6500329d436df5103a6bc465fc393335d7265
Samet Keserci <sammath@users.noreply.github.com>
2015-12-29 18:25:24 -0500

    Create reducer_pySpark.py

---
+++ MapReduce_Examples/WordCount_Spark/reducer_pySpark.py
@@ -0,0 +1,5 @@
+
+def sum_counts(a,b): # With Spark Framework everything is so short and easy
+    return a+b
+    
+# To see how mapper and reducer are called, see the file Run_Spark_for_WorkCount.py
